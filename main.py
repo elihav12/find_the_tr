c6569cad4fbb5de3f7cf25f4eb6445ac1ab169fd
elihav12 <elihavkmarleker9@gmail.com>
2025-11-27 18:46:09 +0200

    print the bord

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import random
 def create_board(size):
     return [["." for _ in range(size)] for _ in range(size)]
 
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+    print()
+
 
