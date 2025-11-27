13bd77211d327b9477c81e93a40514639aed2f54
elihav12 <elihavkmarleker9@gmail.com>
2025-11-27 18:47:38 +0200

    geting the input

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
     print()
-
-
+def get_input(prompt, size):
+    while True:
+        try:
+            value = int(input(prompt))
+            if 1 <= value <= size:
+                return value
+            else:
+                print(f"the gase has to be between 1 to: {size}.")
+        except ValueError:
+            print("it has to be a number")
